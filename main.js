7ddff7c607164363a42d446815bff71609001143
CyberRex <hspwinx86@gmail.com>
2021-03-31 19:03:02 +0900

    First commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const server = require('express')();
+const line = require('@line/bot-sdk');
+
+const line_config = {
+	channelAccessToken: process.env.LINE_ACCESS_TOKEN,
+	channelSecret: process.env.LINE_CHANNEL_SECRET
+};
+
+server.listen(process.env.PORT || 3000);
+
+server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
+
+	req.sendStatus(200);
+	console.log(req.body);
+
+});
