bd88f80655ca9bc66b3cda46cde81b2ebe97b4ea
CyberRex <hspwinx86@gmail.com>
2021-03-31 19:19:44 +0900

    update

--- main.js
+++ main.js
@@ -8,9 +8,24 @@ const line_config = {
 
 server.listen(process.env.PORT || 3000);
 
+const bot = new line.Client(line_config);
+
 server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
 
 	res.sendStatus(200);
 	console.log(req.body);
+
+	let events_processed = [];
+
+	req.body.events.forEach((event) => {
+		if (event.type == 'message' && event.message.type == 'text') {
+			const lns = event.message.text.split('\n');
+			console.log(lns);
+		}
+	});
+
+	Promise.all(events_processed).then((response) => {
+		console.log(`${response.length} event(s) processed.`);
+	});
 
 });
